0dc89fc9d23f3c0484056ddb3c5d056f50a9cc14
Andy Bower <bower@bowerhaus.eu>
2013-07-28 12:17:11 +0100

    no message

--- IosActivitySpinner.lua
+++ IosActivitySpinner.lua
@@ -42,26 +42,35 @@ IosActivitySpinner=Core.class()
 
 function IosActivitySpinner:init(labelText) 
 	IosActivitySpinner.sharedInstance=self
-	local loading=UIView:initWithFrame(CGRect(0, 0, 120, 120))
-	loading:layer():setCornerRadius(15)
-	loading:setOpaque(false)
-	loading:setBackgroundColor(UIColor:colorWithWhite_alpha(0, 0.6))
-	
-	local label=UILabel:initWithFrame(CGRect(20, 20, 81, 22))
-	label:setText(labelText)
-	label:setFont(UIFont:boldSystemFontOfSize(14))
-	label:setTextAlignment(UITextAlignmentCenter)
-	label:setTextColor(UIColor:colorWithWhite_alpha(1, 0.8))
-	label:setBackgroundColor(UIColor:clearColor())
-	loading:addSubview(label)
 	
 	local spinning=UIActivityIndicatorView:initWithActivityIndicatorStyle(UIActivityIndicatorViewStyleWhiteLarge)
 	spinning:setFrame(CGRect(42, 54, 37, 37))
 	spinning:startAnimating()	
-	loading:addSubview(spinning)
 	
-	getRootViewController():view():addSubview(loading)
-	loading:setCenter(CGPoint(application:getContentWidth()/2, application:getContentHeight()/2))
+	if labelText then
+		local loading=UIView:initWithFrame(CGRect(0, 0, 120, 120))
+		loading:layer():setCornerRadius(15)
+		loading:setOpaque(false)
+		loading:setBackgroundColor(UIColor:colorWithWhite_alpha(0, 0.6))
+		
+		local label=UILabel:initWithFrame(CGRect(20, 20, 81, 22))
+		label:setText(labelText)
+		label:setFont(UIFont:boldSystemFontOfSize(14))
+		label:setTextAlignment(UITextAlignmentCenter)
+		label:setTextColor(UIColor:colorWithWhite_alpha(1, 0.8))
+		label:setBackgroundColor(UIColor:clearColor())
+		loading:addSubview(label)
+		loading:addSubview(spinning)
+	else
+		loading=spinning
+	end
+	
+	local rootView=getRootViewController():view()
+	rootView:addSubview(loading)
+	local bounds=rootView:bounds()
+	local w = bounds.width
+	local h = bounds.height
+	loading:setCenter(CGPoint(w/2, h/2))
 	self.spinner=loading
 end
 
@@ -71,12 +80,15 @@ function IosActivitySpinner.show(labelText) --` @public @function
 	-- iOS on a background thread so it doesn't matter if you subsequently block the UI thread; the spinner
 	-- will continue to spin.
 	--
+	IosActivitySpinner.hide()
 	IosActivitySpinner.sharedInstance=IosActivitySpinner.new(labelText)
 end
 
 function IosActivitySpinner.hide() --` @public @function
 	-- Stop any current instance of the [IosActivitySpinner] and remove it from the screen.
-	IosActivitySpinner.sharedInstance.spinner:removeFromSuperview()
-	IosActivitySpinner.sharedInstance=nil
+	if IosActivitySpinner.sharedInstance then
+		IosActivitySpinner.sharedInstance.spinner:removeFromSuperview()
+		IosActivitySpinner.sharedInstance=nil
+	end
 end
 
